4ca481b727535b43838d8c82a105d3f4e193849b
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-05-10 23:29:57 -0300

    Commit services

---
+++ src/services/CellService.js
@@ -0,0 +1,16 @@
+import { Api } from "helpers/Api";
+
+const parseResponse = (response) => response.json();
+
+export const CellService = {
+  getList: () =>
+    fetch(Api.cellList(), { method: "GET" }).then(parseResponse),
+  getById: (id) =>
+    fetch(Api.cellById(id), { method: "GET" }).then(parseResponse),
+  create: () =>
+    fetch(Api.createCell(), { method: "POST" }).then(parseResponse),
+  updtateById: (id) =>
+    fetch(Api.updateCellById(id), { method: "PUT" }).then(parseResponse),
+  deleteById: (id) =>
+    fetch(Api.deleteCellById(id), { method: "DELETE" }).then(parseResponse),
+};
