5acb16ccfb95ce8de5e31593ad4c7b425c2401a7
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-05-10 21:19:28 -0300

    Commit hoocks

--- src/CellList.jsx
+++ src/CellList.jsx
@@ -1,16 +1,25 @@
+import React, { useState } from 'react';
 import { cells } from './mocks/cells.js';
 import './CellList.css';
+
 function CellList() {
+  const [selectedCell, setSelectedCell] = useState({});
+  const itemAdd = (cellIndex) => {
+    const cell = { [cellIndex]: Number(selectedCell[cellIndex] || 0) + 1 };
+    setSelectedCell({ ...selectedCell, ...cell});
+  };
+
   return (
     <div className="CellList">
       {cells.map((cell, index) => (
         <div className="CellListItem" key={`CellListItem-${index}`}>
+          <span className='CellListItem__badge'>{selectedCell[index] || 0}</span>
           <div>
             <div className="CellListItem__name">{cell.name}</div>
             <div className="CellListItem__price">{cell.price.toFixed(2)}</div>
             <div className="CellListItem__description">{cell.description}</div>
             <div className="CellListItem__actions Actions">
-              <button className="Actions__add Actions__add-fill ">
+              <button className="Actions__add Actions__add-fill " onClick={()=> itemAdd(index)}>
                 Adicionar
               </button>
             </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,26 @@
-import "./Home.css";
-import CellList from "CellList";
+import './Home.css';
+import CellList from 'CellList';
+import sacola from '../assets/icons/sacola.svg';
+import shop from '../assets/shop.svg';
 
 function Home() {
   return (
     <div className="Home">
+      <div className="Home__header Header">
+        <div className="row">
+          <div className="Header__logo Logo">
+            <img
+              src={shop}  width="70px"  alt="Logo MarketPlace" className="Logo__icone"/>
+            <span className="Logo__titulo"> Kohaute Place </span>
+          </div>
+          <div className="Header__opcoes Opcoes">
+            <div className="Opcoes__sacola Sacola">
+              <img
+                src={sacola} width="40px" className="Sacola__icone"  alt="Sacola de compras" />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="Home__container">
         <CellList />
       </div>
