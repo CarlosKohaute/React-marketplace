14dec22c0876d131940fea011715cdd0d4b87914
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-06-06 19:35:10 -0300

    Commit memory

---
+++ kohaute-marketplace/src/mocks/cells.js
@@ -0,0 +1,74 @@
+export const cells = [
+  {
+    name: 'Xiaomi 12 PRO',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/xiaomi12pro.png'),
+    price: 4000,
+    poweredby: 'Mi',
+  },
+  {
+    name: 'Iphone 13 Pro',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/iphone13pro.png'),
+    price: 7000,
+    poweredby: 'Apple',
+  },
+  {
+    name: 'S22 Ultra',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/s22ultra.png'),
+    price: 7000,
+    poweredby: 'Samsung',
+  },
+  {
+    name: 'Xiaomi 12 PRO',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/xiaomi12pro.png'),
+    price: 4000,
+    poweredby: 'Mi',
+  },
+  {
+    name: 'Iphone 13 Pro',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/iphone13pro.png'),
+    price: 7000,
+    poweredby: 'Apple',
+  },
+  {
+    name: 'S22 Ultra',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/s22ultra.png'),
+    price: 7000,
+    poweredby: 'Samsung',
+  },
+  {
+    name: 'Xiaomi 12 PRO',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/xiaomi12pro.png'),
+    price: 4000,
+    poweredby: 'Mi',
+  },
+  {
+    name: 'Iphone 13 Pro',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/iphone13pro.png'),
+    price: 7000,
+    poweredby: 'Apple',
+  },
+  {
+    name: 'S22 Ultra',
+    Description:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    photo: require('public/assets/images/s22ultra.png'),
+    price: 7000,
+    poweredby: 'Samsung',
+  },
+];
