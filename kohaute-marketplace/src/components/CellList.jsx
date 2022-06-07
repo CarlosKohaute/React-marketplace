0b38536b2c5e567715b275a5e0ae706fa4f7c66f
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-06-06 21:42:59 -0300

    Commit remove button

--- kohaute-marketplace/src/components/CellList.jsx
+++ kohaute-marketplace/src/components/CellList.jsx
@@ -5,16 +5,27 @@ import './CellList.css';
 function CellList() {
   const [chosedCell, setChosedCell] = useState({});
 
+  const itemRemove = (cellIndex) => {
+    const cell = { [cellIndex]: Number(chosedCell[cellIndex] || 0) -1 }
+    setChosedCell({...chosedCell, ...cell});
+}
+
+  const badgeCounter = (canRender, index) =>
+	Boolean(canRender) && (<span className="CellListItem__badge"> {chosedCell[index]} </span>);
+
   const itemAdd = (cellIndex) => {
           const cell = { [cellIndex]: Number(chosedCell[cellIndex] || 0) +1 }
           setChosedCell({ ...chosedCell, ...cell});
   }
 
+  const removeButton = (canRender, index) =>
+	Boolean(canRender) && (<button className="Actions__remove" onClick={() => itemRemove(index)}>remover</button>);
+
   return (
     <div className="CellList">
       {cells.map((cell, index) =>(
       <div className="CellListItem"   key={`CellListItem-${index}`}>
-        	<span className="CellListItem__badge"> {chosedCell[index] || 0} </span>
+        	{badgeCounter(chosedCell[index], index)}
 
         <div>
           <div className="CellListItem__name">{cell.name}</div>
@@ -23,9 +34,10 @@ function CellList() {
           {cell.description}
           </div>
           <div className="CellListItem__actions Actions">
-            <button className="Actions__add Actions__add--fill" onClick={() => itemAdd(index)}>
+            <button className={`Actions__add ${!chosedCell[index] && "Actions__add--fill"}`}onClick={() => itemAdd(index)}>
               Adicionar
             </button>
+            {removeButton(chosedCell[index], index)}
           </div>
         </div>
         <img className="CellListItem__photo" src={cell.photo} alt={cell.name} />
