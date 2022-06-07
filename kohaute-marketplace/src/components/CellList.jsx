fcf344ef1059e3522796163d288816136d26fabd
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-06-06 21:05:47 -0300

    Commit hocks

--- kohaute-marketplace/src/components/CellList.jsx
+++ kohaute-marketplace/src/components/CellList.jsx
@@ -1,11 +1,21 @@
 import { cells } from 'mocks/cells';
+import React, {useState} from 'react';
 import './CellList.css';
 
 function CellList() {
+  const [chosedCell, setChosedCell] = useState({});
+
+  const itemAdd = (cellIndex) => {
+          const cell = { [cellIndex]: Number(chosedCell[cellIndex] || 0) +1 }
+          setChosedCell({ ...chosedCell, ...cell});
+  }
+
   return (
     <div className="CellList">
       {cells.map((cell, index) =>(
       <div className="CellListItem"   key={`CellListItem-${index}`}>
+        	<span className="CellListItem__badge"> {chosedCell[index] || 0} </span>
+
         <div>
           <div className="CellListItem__name">{cell.name}</div>
           <div className="CellListItem__price">R$ {cell.price.toFixed(2)}</div>
@@ -13,7 +23,7 @@ function CellList() {
           {cell.description}
           </div>
           <div className="CellListItem__actions Actions">
-            <button className="Actions__add Actions__add--fill">
+            <button className="Actions__add Actions__add--fill" onClick={() => itemAdd(index)}>
               Adicionar
             </button>
           </div>
--- kohaute-marketplace/src/components/Home.jsx
+++ kohaute-marketplace/src/components/Home.jsx
@@ -1,12 +1,29 @@
 import CellList from './CellList';
-import './Home.css'
+import './Home.css';
+import sacola from '../assets/icons/sacola.svg';
+import shop from '../assets/shop.svg'
 
 function Home() {
-  return <div className="Home">
-    <div className="Home__container">
-      <CellList/>
+  return (
+    <div className="Home">
+      	<div className="Home__header Header">
+		<div className="row">
+			<div className="Header__logo Logo">
+				<img src={shop} width="70px" alt="Logo kohaute MarketPlace" className="Logo__icon" />
+				<span className="Logo__tittle"> Kohaute MarketPlace </span>
+			</div>
+			<div className="Header__options Options">
+				<div className="Options__bag Bag">
+					<img src={sacola} width="40px" className="Bag__icon" alt="Sacola de compras" />
+				</div>
+			</div>
+		</div>
+	</div>
+      <div className="Home__container">
+        <CellList />
       </div>
-  </div>;
+    </div>
+  );
 }
 
 export default Home;
