27dc9bb763a4bb04daec2a40742817345d79c02b
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-06-08 00:56:09 -0300

    Commit public images

--- kohaute-marketplace/src/services/CellService.js
+++ kohaute-marketplace/src/services/CellService.js
@@ -3,15 +3,12 @@ import { Api } from 'helpers/Api';
 const parseResponse = (response) => response.json();
 
 export const CellService = {
-  getList: () => 
-      fetch(Api.cellList(), { method: 'GET' }).then(parseResponse),
-  getById:(id)=>
-      fetch(Api.cellById(id), { method: 'GET' }).then(parseResponse),
-      create:()=>
-      fetch(Api.createCell(), { method: 'POST' }).then(parseResponse),
-      updateById:(id)=>
-      fetch(Api.updateCellById(id), { method: 'PUT' }).then(parseResponse),
-      deleteById:(id)=>
-      fetch(Api.deleteCellById(id), { method: 'DELETE' }).then(parseResponse),
-
+  getList: () => fetch(Api.cellList(), { method: 'GET' }).then(parseResponse),
+  getById: (id) =>
+    fetch(Api.cellById(id), { method: 'GET' }).then(parseResponse),
+  create: () => fetch(Api.createCell(), { method: 'POST' }).then(parseResponse),
+  updateById: (id) =>
+    fetch(Api.updateCellById(id), { method: 'PUT' }).then(parseResponse),
+  deleteById: (id) =>
+    fetch(Api.deleteCellById(id), { method: 'DELETE' }).then(parseResponse),
 };
